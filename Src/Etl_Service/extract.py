e575447d1258a0f38d8262599c2897f08c336799
Muhammad Arsalan <muhammad.arsalan@xloopdigital.com>
2023-06-14 16:32:42 +0500

    updated

--- Src/Etl_Service/extract.py
+++ Src/Etl_Service/extract.py
@@ -11,23 +11,6 @@ def fetch_data_from_apis(api_url):
         print("An error occurred:", e)
         return None
 
-def extract():
-    
-    data = {}
-
-    for k, v in api_urls.items():
-        data[k] = fetch_data_from_apis(v)
-        
-    spark = SparkSession.builder.getOrCreate()
-
-    df1 = spark.createDataFrame(data["appointment_api"])
-    df2 = spark.createDataFrame(data["councillor_api"])
-    df3 = spark.createDataFrame(data["patient_councillor_api"])
-    df4 = spark.createDataFrame(data["rating_api"])
-
-    return df1, df2, df3, df4
-    spark.stop()
-
 # Define the API URLs with names
 api_urls = {
     "appointment_api": "https://xloop-dummy.herokuapp.com/appointment",
